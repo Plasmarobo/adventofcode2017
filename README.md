c23c8d657babafbbe64746ab53d7a5aecdf1201a
Austen Higgins-Cassidy <austen@thelevelup.com>
2017-12-01 11:19:39 -0500

    Nicer Day1 Part 2

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ I use a simple trick (ASCII/Unicode encoding exploit) to avoid calling atoi.
 Subtracting zero puts a character in the range 0-9 into a numerical context.
 No tests are written, no input is sanitized. Very naive implementation.
 ### Part 2
-The previous implementation was not reusable. It was replaced with a ringbuffer implementation with a second comparison iterator. It can technically start anywhere, but will advance in lockstep with the primary iterator.
+The previous implementation was not reusable. It was replaced with a ringbuffer implementation with a second comparison iterator. Start point and advancement are somewhat configurable, but require a rebuild.
 
 An extension would make the iterators overridable classes, and supply a difference starting point. This model would allow slower/faster iterators as well.
+A configuration file would also improve this model.
--- day1.5.cpp
+++ day1.5.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <string.h>
 
+// How to initialize the index pointer
+#define INDEX_AT 0
+// How to initialize the comparison pointer
+#define COMPARE_AT length / 2
+// Advances the index pointer
+#define ADVANCE_INDEX(x) ++x
+// Advances the comparison pointer
+#define ADVANCE_COMPARE(x) ++x
+
 int main(int argc, char** argv) {
 	if (argc != 2) {
 		std::cout << "Usage: " << argv[0] << " [0-9]+" << std::endl;
@@ -10,30 +19,26 @@ int main(int argc, char** argv) {
 	uint32_t length = strlen(argv[1]);
 	uint32_t sum = 0;
 
-	std::cout << "Length is " << length << std::endl;	
-
 	if (length == 0) {
 		return 0;
 	}
 
-	uint32_t i = 0;
-	uint32_t j = length / 2; // Even number of elements
-
-	std::cout << "Starting with " << i << " and " << j << std::endl;
+	uint32_t index_pointer = INDEX_AT;
+	uint32_t comparison_pointer = COMPARE_AT; 
 
 	do {
-		if (argv[1][i] == argv[1][j]) {
-			sum += argv[1][i] - '0';
+		if (argv[1][index_pointer] == argv[1][comparison_pointer]) {
+			sum += argv[1][index_pointer] - '0';
 		}		
-		i += 1;
-		j += 1;
-		if (j >= length) {
-			j = 0;
+		ADVANCE_INDEX(index_pointer);
+		ADVANCE_COMPARE(comparison_pointer);
+		if (comparison_pointer >= length) {
+			comparison_pointer = 0;
 		}
-		if (i >= length) {
-			i = 0;
+		if (index_pointer >= length) {
+			index_pointer = 0;
 		}
-	} while(i != 0);
+	} while(i != INDEX_AT);
 	
 	std::cout << "Sum is " << sum << std::endl;
 	

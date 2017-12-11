abd4dbbbbb39a771cef96abe8e397fbdad688e71
Austen Higgins-Cassidy <austen@thelevelup.com>
2017-12-11 13:49:22 -0500

    Day 11

---
+++ day11.rb
@@ -0,0 +1,44 @@
+
+class HexMap
+
+  def initialize()
+    @x = 0
+    @y = 0
+    @z = 0
+  end
+
+  def translate_step(dir)
+    case dir
+    when "ne"
+      @x += 1
+      @z -= 1
+    when "n"
+      @y += 1
+      @z -= 1
+    when "nw"
+      @x -= 1
+      @y += 1
+    when "sw"
+      @x -= 1
+      @z += 1
+    when "s"
+      @y -= 1
+      @z += 1
+    when "se"
+      @x += 1
+      @y -= 1
+    end
+  end
+
+  def print_position()
+    print "At #{@x},#{@y}\n"
+    print "#{[@x,@y,@z].max} from 0,0\n"
+  end
+end
+
+File.open(ARGV[0], "r").read.each_line do |line|
+  moves = line.chomp.split(",")
+  map = HexMap.new
+  moves.each {|move| map.translate_step(move)}
+  map.print_position()
+end
